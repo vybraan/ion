20e726621c0ea4252fb6efeb53da6aee01390e76
Ailton Baúque <ailtonbauque@outlook.com>
2025-11-22 07:27:13 +0200

    feat: add base event and encode/decode functions

--- protocol/events.go
+++ protocol/events.go
@@ -1,18 +1,45 @@
 package protocol
 
+import "encoding/json"
+
 type EventType string
 
 const (
+	EventDescribe EventType = "describe"
 	EventReady    EventType = "ready"
 	EventStart    EventType = "start"
 	EventStop     EventType = "stop"
 	EventError    EventType = "error"
-	EventAudio    EventType = "audio"
-	EventDescribe EventType = "describe"
 )
 
+const (
+	EventAudio EventType = "audio"
+)
+
+type BaseEvent struct {
+	Type EventType `json:"type"`
+}
+
 type ReadyEvent struct {
-	Type         EventType `json:"type"`
-	Protocol     string    `json:"protocol"`
-	AudioFormats []string  `json:"audio_formats,omitempty"`
+	Type       EventType `json:"type"`
+	Protocol   string    `json:"protocol"`
+	SampleRate int       `json:"sample_rate"`
+	Channels   int       `json:"channels"`
+	Format     string    `json:"format"`
+}
+
+type StartEvent struct {
+	Type EventType `json:"type"`
+}
+
+type StopEvent struct {
+	Type EventType `json:"type"`
+}
+
+func Encode(v any) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func Decode(data []byte, v any) error {
+	return json.Unmarshal(data, v)
 }
